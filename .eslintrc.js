53ecab90491c153385c7d37e6be1ad579d1feb6b
Mateusz Słotwiński <mateuszslotwinski16@gmail.com>
2025-01-28 20:32:56 +0100

    aaaaa

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,25 @@
-module.exports = {
+const eslintConfig = {
   root: true,
   env: {
     node: true,
   },
   extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "plugin:prettier/recommended",
+    'plugin:vue/vue3-essential',
+    'plugin:prettier/recommended',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
   ],
   parserOptions: {
     ecmaVersion: 2020,
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    'no-console': 'warn',
+    'no-debugger': 'warn',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    'explicit-module-boundary-types': 'off',
+    'prettier/prettier': [1, { endOfLine: 'auto' }, { usePrettierrc: true }],
+    'vue/multi-word-component-names': 'off',
   },
-};
+}
+
+module.exports = eslintConfig
---
+++ src/views/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  Tabliczka mnozenia dla maniakulka
+  <div class="home">
+    Wpisz {{ x }} x {{ y }}
+    <input type="number" v-model="guess" @keyup="Next()" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  data() {
+    return {
+      x: 0,
+      y: 0,
+      guess: '',
+    }
+  },
+  methods: {
+    Rand(): number {
+      return Math.floor(Math.random() * 10)
+    },
+    Next() {
+      if (Number(this.guess) == this.x * this.y) {
+        this.guess = ''
+        this.x = this.Rand()
+        this.y = this.Rand()
+      }
+    },
+  },
+  mounted() {
+    this.Next()
+  },
+})
+</script>
