8b0668acd51679cce9854cc6111cd7b7ab1809fc
Mateusz Słotwiński <mateuszslotwinski16@gmail.com>
2025-02-06 22:07:31 +0100

    abc

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,88 @@
 <template>
-  <nav><router-link to="/">Hei</router-link></nav>
   <router-view />
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import url('https://fonts.googleapis.com/css2?family=Mooli&family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+
+$main: #a50034;
+$main2: #ee70a7;
+$violet: #62477a;
+$light: #e3e3e3;
+$gray: #c6cbcc;
+$dark_gray: #7c888c;
+$dark: #28353a;
+$black: #1b2023;
+$bg: #09151e;
+
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+  position: relative;
 }
 
-nav {
-  padding: 30px;
+*::-moz-selection {
+  /* Code for Firefox */
+  background: #00a3cc !important;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+*::selection {
+  background: #00a3cc !important;
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+html {
+  font-family: 'Rubik', sans-serif !important;
+}
+
+body {
+  padding: 0px !important;
+  margin: 0px !important;
+  overflow-x: hidden;
+  background-color: #09151e;
+  min-height: 100vh;
+  color: #cdcdcd;
+  font-family: 'Rubik', sans-serif !important;
+}
+
+a {
+  text-decoration: none;
+  color: #cdcdcd;
+  cursor: pointer;
+}
+
+input,
+button,
+select,
+textarea {
+  font-family: 'Rubik', sans-serif;
+  border: none;
+  background-color: #e3e3e3;
+  color: #333;
+  padding: 0;
+}
+
+button,
+select {
+  cursor: pointer;
+}
+
+button:focus,
+input:focus,
+select:focus,
+textarea:focus {
+  outline: 0 !important;
+}
+
+::-webkit-scrollbar {
+  width: 0.5rem;
+}
+::-webkit-scrollbar-thumb {
+  background: linear-gradient($main, $main2);
+  border-radius: 4px;
+}
+
+img {
+  pointer-events: none;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,28 @@
 <template>
-  Tabliczka mnozenia dla maniakulka
-  <div class="home">
-    Wpisz {{ x }} x {{ y }}
-    <input type="number" v-model="guess" @keyup="Next()" />
+  <div class="main">
+    <div class="hi">
+      <div>Tabliczka mnozenia</div>
+      dla <span>maniakulka</span>
+    </div>
+    <div class="home">
+      Wpisz <span>{{ x }} x {{ y }}</span>
+      <!-- <input type="number" v-model="guess" @keyup="Next()" /> -->
+    </div>
+    <div class="field" v-html="guess" />
+    <div class="buttons">
+      <div
+        class="button"
+        :key="nr"
+        v-for="nr in [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]"
+        @click="Add(nr)"
+      >
+        {{ nr }}
+      </div>
+      <div class="button" @click="Pop()">X</div>
+    </div>
+    <img
+      src="https://img-09.stickers.cloud/packs/88472377-b658-4926-adc7-2829da602644/webp/02e0dea0-1dbe-48aa-96c0-b010864a40de.webp"
+    />
   </div>
 </template>
 
@@ -18,6 +38,14 @@ export default defineComponent({
     }
   },
   methods: {
+    Add(n: number) {
+      this.guess = this.guess + String(n)
+      this.Next()
+    },
+    Pop() {
+      this.guess = this.guess.slice(0, -1)
+      this.Next()
+    },
     Rand(): number {
       return Math.floor(Math.random() * 10)
     },
@@ -34,3 +62,69 @@ export default defineComponent({
   },
 })
 </script>
+
+<style lang="scss" scoped>
+.main {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  flex-grow: 1;
+  height: 100vh;
+}
+.hi {
+  font-size: 23px;
+  text-align: center;
+  margin-bottom: 10px;
+}
+
+$main: #a50034;
+$main2: #ee70a7;
+
+span {
+  color: $main2;
+}
+
+.home {
+  font-size: 20px;
+}
+
+.field {
+  margin: 10px;
+  font-size: 20px;
+  height: 40px;
+  width: 300px;
+  background-color: #c6cbcc;
+  color: $main;
+  border-radius: 10px;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  padding-right: 10px;
+}
+
+.button {
+  background: $main2;
+  width: 70px;
+  height: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 20px;
+  margin: 5px;
+  flex-grow: 1;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+.buttons {
+  display: flex;
+  flex-wrap: wrap;
+  max-width: 260px;
+}
+
+img {
+  width: 100px;
+  margin-top: 10px;
+}
+</style>
